9f8e4fe5fd615e8f6db2387ee4d75f72dfe52f88
Anirudh Vaish <47653244+avaish1409@users.noreply.github.com>
2020-04-04 15:55:48 +0530

    update

--- q1.c
+++ q1.c
@@ -5,39 +5,41 @@
 int main() {
 
 	int n,t;
-
-	scanf("%d",&t);
-
+	scanf("%d",&t);											//Test Cases	
 	for(int i=0;i<t;i++){
 		scanf("%d",&n);
 		int a[n];
 		for(int j=0;j<n;j++){
 			scanf("%d",&a[i]);
 		}
-	int k=0;
-	int flag=0;
-	while(k < n){
-		int count = 1;
-		if(a[k]==1){
-			int l = k;
-			l++;
-			while(a[l]!=1){
-			l++;
-			count++;
-		} 
-		k= l;
+		printf("inp: %d",n);
+		int k=0;
+		int flag=0;
+		while(k < n){
+			int count = 1;
+			if(a[k]==1){
+				int l = k;
+				l++;
+				while(a[l]!=1 && l<n){
+					l++;
+					count++;
+					printf("cn++\n");
+				} 
+				k= l;
+				printf("l: %d",l);
+			}
+			if(count<5 && count!=1 && k!=n-1){
+				printf("No: %d\n\n",count);
+				flag=1;
+				break;
+			}
+			k++;
+			printf("cnt: %d\n",count);
 		}
-		if(count>=5){
-			printf("YES");
-			flag=1;
-			break;
-		}
-	}
 		if(flag==0){
-			printf("No");
+			printf("Yes\n\n");
 		}
 	}
 
-return 0;
-
+	return 0;
 }
