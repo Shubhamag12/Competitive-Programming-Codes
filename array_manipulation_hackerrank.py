4f0aba1bbad380fac76973abc0ca283ad5fb9379
Shubham Agarwal <49315586+Shubhamag12@users.noreply.github.com>
2020-08-12 23:43:20 +0530

    Update array_manipulation_hackerrank.py

--- array_manipulation_hackerrank.py
+++ array_manipulation_hackerrank.py
@@ -1,2 +1,19 @@
 # https://www.hackerrank.com/challenges/crush/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
 
+n, m = map(int, input().split())
+v = [0] * (n + 1)
+
+for _ in range(m):
+    a, b, k = map(int, input().split())
+    v[a] += k
+    if b + 1 <= n:
+        v[b+1] -= k
+        
+maximum_sum = 0
+sum = 0
+
+for i in v:
+    sum += i
+    maximum_sum = max(maximum_sum, sum)
+
+print(maximum_sum)    
