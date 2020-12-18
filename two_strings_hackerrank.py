b27511c4145bd4dfe9d4ebe89bef00ab13e1531b
Shubham Agarwal <49315586+Shubhamag12@users.noreply.github.com>
2020-12-18 14:26:16 +0530

    Delete two_strings_hackerrank.py

--- two_strings_hackerrank.py
+++
@@ -1,9 +0,0 @@
-# https://www.hackerrank.com/challenges/two-strings/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
-
-n = int(input())
-
-for _ in range(n):
-    s1 = set(input())
-    s2 = set(input())
-
-    print('YES' if s1.intersection(s2) else 'NO')
