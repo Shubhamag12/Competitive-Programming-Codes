d882121bf970d9139c8a8393461f831db3367554
Shubham Agarwal <49315586+Shubhamag12@users.noreply.github.com>
2020-12-18 14:26:06 +0530

    Delete unique_paths.py

--- unique_paths.py
+++
@@ -1,9 +0,0 @@
-#https://leetcode.com/problems/unique-paths/
-
-class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        a = [[1 for i in range(m)] for j in range(n)]
-        for i in range(1,n):
-            for j in range(1,m):
-                a[i][j] = a[i-1][j] + a[i][j-1]
-        return a[n-1][m-1]
