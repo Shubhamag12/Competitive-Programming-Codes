5ee243a25ad1c9224760657b3de4ae2ac18140e8
Shubham Agarwal <49315586+Shubhamag12@users.noreply.github.com>
2021-01-03 01:06:55 +0530

    LeetCode - Split a String in Balanced Strings

--- Leetcode/Split_String.java
+++ Leetcode/Split_String.java
@@ -4,17 +4,13 @@ class Solution {
     public int balancedStringSplit(String s) {
         int sum = 0;
         int count = 0;
-        int[] a = new int[s.length()];
-        for(int i = 0; i < a.length; i++){
+        for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == 'R'){
-                a[i] = 1;
+                sum += 1;
             }
             else{
-                a[i] = -1;
+                sum += -1;
             }
-        }
-        for(int i = 0; i < a.length; i++){
-            sum += a[i];
             if(sum == 0){
                 count++;
             }
