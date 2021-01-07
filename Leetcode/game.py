9dbdf8d8813f52e0b384927004330fbd233f1d8f
Shubham Agarwal <49315586+Shubhamag12@users.noreply.github.com>
2021-01-07 14:15:08 +0530

    Delete game.py

--- Leetcode/game.py
+++
@@ -1,9 +0,0 @@
-# https://leetcode.com/problems/jump-game/
-
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        l = len(nums) - 1
-        for i in reversed(range(len(nums))):
-            if(i + nums[i] >= l):
-                l = i
-        return l == 0        
